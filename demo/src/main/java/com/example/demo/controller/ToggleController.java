3c10a260d6d78f14ff075af937b1c050884df5f0
Makulabindupriya <bindupriya1303@gmail.com>
2021-11-25 14:05:00 +0530

    Added html & css files

--- demo/src/main/java/com/example/demo/controller/ToggleController.java
+++ demo/src/main/java/com/example/demo/controller/ToggleController.java
@@ -1,23 +1,28 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.ast.VariableReference;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.component.ToggleComponent;
 
 import no.finn.unleash.UnleashContext;
 
-@RestController
+@Controller
 //@RequestMapping("/project")
 public class ToggleController {
 
 	@Autowired
 	private ToggleComponent toggleComponent;
 	
-	@GetMapping("/toggle/default")
+	@GetMapping("toggle/default")
 	public Boolean getToggleStatus() {
 		System.out.println("con.method()");
 		return toggleComponent.isDefaultToggleActive("Tester-Toggle");
@@ -38,6 +43,46 @@ public class ToggleController {
 	@GetMapping("toggle/features/{toggleName}")
 	public String getToggleStatus(@PathVariable String toggleName) {
 		System.out.println("In getToggleStatus(), pathVariable: " + toggleName);
+		
 		return "Toggle Name: " + toggleName + ", Status: " + toggleComponent.isDefaultToggleActive(toggleName);
 	}
+	
+	
+	@GetMapping("/toggle/beta")
+	public boolean getBetaToggleStatus() {
+		UnleashContext context = UnleashContext.builder().userId("bindu").build();
+		return toggleComponent.isBetaToggleActive("demo-beta", context);
+	}
+	
+	
+	@RequestMapping("/window")
+	public ModelAndView welcome() {
+		ModelAndView modelAndView = new ModelAndView();
+	    modelAndView.setViewName("welcome.html");
+	    return modelAndView;
+	}
+	
+		
+	@GetMapping("/")
+	public String Welcome(Model model) { 
+		boolean betaValue = toggleComponent.isDefaultToggleActive("demo-beta");
+		boolean documentationValue = toggleComponent.isDefaultToggleActive("documentation");
+		boolean demoValue = toggleComponent.isDefaultToggleActive("demonstration");
+		boolean contactValue = toggleComponent.isDefaultToggleActive("contact");
+		boolean toggleImg = toggleComponent.isDefaultToggleActive("toggleImg");
+		
+		model.addAttribute("Beta", betaValue);
+		model.addAttribute("Documentation", documentationValue);
+		model.addAttribute("Contact", contactValue);
+		model.addAttribute("Demo", demoValue);
+		model.addAttribute("ToggleImg", toggleImg);
+		
+		return "dashboard.html";
+		
+	}
+
+	
+	
+	
+	
 }
